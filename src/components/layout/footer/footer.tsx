325e12134dbf2b3302bfac7f6ed6c776a9386765
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2024-01-26 21:52:05 +0530

    submit

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -31,17 +31,18 @@ const Footer = () => {
                     <section>
                         <div>
                             <h3 className={'footer-MainText'}>Follow Us</h3>
-                            <aside>
-                                <i>*</i>
-                                <i>*</i>
-                                <i>*</i>
-                                <i>*</i>
+                            <aside className={'d-flex justify-content-around align-items-center'}>
+                                <img className={'socialMedia'} src="src/assets/facebook (2).png" alt="alt"/>
+                                <img className={'socialMedia'} src="src/assets/linkedin (3).png" alt="alt2"/>
+                                <img className={'socialMedia'} src="src/assets/twitter (1).png" alt="alt3"/>
                             </aside>
                         </div>
                         <div>
                             <h3 className={'footer-MainText'}>Get in your inbox</h3>
-                            <div>
-                                <button>Email</button>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Type Your Email"
+                                       aria-label="Example text with button addon" aria-describedby="button-addon1"/>
+                                <button className="btn btn-dark" type="button" id="button-addon1">SEND</button>
                             </div>
                             <div>
                                 <p></p>
